5ad1c2d2a501800ad8ff018df260eea50ed220b4
vinicius almeida <viniciusalmeidaa_@outlook.com>
2023-08-15 16:54:07 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-/*import Home from "./components/pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 import Users from "./components/pages/Users";
-import UserBlog from "./components/pages/UserBlog";*/
+import UserBlog from "./components/pages/UserBlog";
 import PostForm from "./components/pages/PostForm";
 
-import "./styles/fontawesome.min.css"
-import "./styles/main.css"
-import "./styles/normalize.css"
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
+import "./styles/normalize.css";
 
 function App() {
-  return <PostForm />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/:userId/post" element={<PostForm />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
